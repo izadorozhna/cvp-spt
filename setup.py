afd64a9efbb3300f81cae2bcdc6a1e6dfd6ac9a1
Oleksii Zhurba <ozhurba@mirantis.com>
2018-06-26 15:59:43 -0500

    Add setup.py and LICENSE files
    Change-Id: I800c7fe6f6bea6644e30c2136d702f1df8de1be0

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_requirements_list(requirements):
+    all_requirements = read(requirements)
+    return all_requirements
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='cvp-spt',
+    version='0.1',
+    description='set of tests for MCP SPT',
+    long_description=readme,
+    author='Mirantis',
+    license=license,
+    install_requires=get_requirements_list('./requirements.txt'),
+    packages=find_packages(exclude=('tests', 'docs'))
+)
