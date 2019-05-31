7fa91bbd44ad62df6643f094edf920b22e0224a2
root <root@cfg01.jawsd.jiocloud.com>
2019-05-31 22:56:53 +0530

    Added offline iperf deb package

--- cvp_spt/utils/ssh.py
+++ cvp_spt/utils/ssh.py
@@ -136,5 +136,10 @@ class prepare_iperf(object):
         config = utils.get_configuration()
         preparation_cmd = config.get('iperf_prep_string') or ['']
         transport.exec_command(preparation_cmd)
-        transport.exec_command('sudo apt-get update; sudo apt-get install -y iperf')
+        try:
+            transport.exec_command('sudo apt-get update; sudo apt-get install -y iperf')
+        except Exception:
+            transport.put_file("/var/lib/cvp-spt/iperf_2.0.5+dfsg1-2_amd64.deb", "/home/ubuntu/iperf_2.0.5+dfsg1-2_amd64.deb")
+            transport.exec_command('sudo dpkg -i /home/ubuntu/iperf_2.0.5+dfsg1-2_amd64.deb')
+        transport.exec_command("sudo ifconfig ens3 mtu 9100")
         transport.exec_command('nohup iperf -s > file 2>&1 &') 
